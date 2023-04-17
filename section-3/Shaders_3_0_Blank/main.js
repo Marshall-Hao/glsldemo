d855ec34c03dd04913f897cfab775a9fb4520d85
Marshall Chan <marshallchan666@outlook.com>
2023-04-18 04:06:41 +0800

    feat: addressing, wrapS,wrapT mode, * -1 => mod 1 1-> 0 0-> 1 reverse

--- section-3/Shaders_3_0_Blank/main.js
+++ section-3/Shaders_3_0_Blank/main.js
@@ -1,9 +1,7 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.136';
-
+import * as THREE from "https://cdn.skypack.dev/three@0.136";
 
 class SimonDevGLSLCourse {
-  constructor() {
-  }
+  constructor() {}
 
   async initialize() {
     this.threejs_ = new THREE.WebGLRenderer({
@@ -12,41 +10,61 @@ class SimonDevGLSLCourse {
     this.threejs_.shadowMap.enabled = true;
     this.threejs_.shadowMap.type = THREE.PCFSoftShadowMap;
     this.threejs_.setPixelRatio(window.devicePixelRatio);
-    this.threejs_.setSize(window.innerWidth, window.innerHeight);
+    this.threejs_.setSize(
+      window.innerWidth,
+      window.innerHeight
+    );
 
     document.body.appendChild(this.threejs_.domElement);
 
-    window.addEventListener('resize', () => {
-      this.onWindowResize_();
-    }, false);
+    window.addEventListener(
+      "resize",
+      () => {
+        this.onWindowResize_();
+      },
+      false
+    );
 
-    this.camera_ = new THREE.OrthographicCamera(0, 1, 1, 0, 0.1, 1000);
+    this.camera_ = new THREE.OrthographicCamera(
+      0,
+      1,
+      1,
+      0,
+      0.1,
+      1000
+    );
     this.camera_.position.set(0, 0, 1);
 
     this.scene_ = new THREE.Scene();
 
     await this.setupProject_();
-    
+
     this.raf_();
   }
 
   async setupProject_() {
-    const vsh = await fetch('./shaders/vertex-shader.glsl');
-    const fsh = await fetch('./shaders/fragment-shader.glsl');
+    const vsh = await fetch("./shaders/vertex-shader.glsl");
+    const fsh = await fetch(
+      "./shaders/fragment-shader.glsl"
+    );
 
     const loader = new THREE.TextureLoader();
-    const dogTexture = loader.load('./textures/dog.jpg');
+    const dogTexture = loader.load("./textures/dog.jpg");
+    // *  S T  - > u v respectively
+    // * ClampToEdgeWrapping 边缘极限拉伸 stretch out
     dogTexture.wrapS = THREE.RepeatWrapping;
     dogTexture.wrapT = THREE.RepeatWrapping;
     dogTexture.magFilter = THREE.NearestFilter;
-    const overlayTexture = loader.load('./textures/overlay.png');
+    const overlayTexture = loader.load(
+      "./textures/overlay.png"
+    );
 
     const geometry = new THREE.PlaneGeometry(1, 1);
     const material = new THREE.ShaderMaterial({
       uniforms: {
-        diffuse: {value: dogTexture},
-        overlay: {value: overlayTexture},
-        tint: {value: new THREE.Vector4(1, 0, 0, 1)},
+        diffuse: { value: dogTexture },
+        overlay: { value: overlayTexture },
+        tint: { value: new THREE.Vector4(1, 0, 0, 1) },
       },
       vertexShader: await vsh.text(),
       fragmentShader: await fsh.text(),
@@ -54,11 +72,14 @@ class SimonDevGLSLCourse {
 
     const plane = new THREE.Mesh(geometry, material);
     plane.position.set(0.5, 0.5, 0);
-    this.scene_.add( plane );
+    this.scene_.add(plane);
   }
 
   onWindowResize_() {
-    this.threejs_.setSize(window.innerWidth, window.innerHeight);
+    this.threejs_.setSize(
+      window.innerWidth,
+      window.innerHeight
+    );
   }
 
   raf_() {
@@ -69,10 +90,9 @@ class SimonDevGLSLCourse {
   }
 }
 
-
 let APP_ = null;
 
-window.addEventListener('DOMContentLoaded', async () => {
+window.addEventListener("DOMContentLoaded", async () => {
   APP_ = new SimonDevGLSLCourse();
   await APP_.initialize();
 });
--- section-3/Shaders_3_0_Blank/shaders/fragment-shader.glsl
+++ section-3/Shaders_3_0_Blank/shaders/fragment-shader.glsl
@@ -8,9 +8,14 @@ void main(void) {
   //  * 2D texture return rgba
   // * flip out
   // vec4 diffuseSample = texture2D(diffuse,(1.0 - vUvs));
-  vec4 diffuseSample = texture2D(diffuse,vUvs);
-  vec4 overlaySample = texture2D(overlay,vUvs);
+  // vec4 diffuseSample = texture2D(diffuse,vUvs);
+  // vec4 overlaySample = texture2D(overlay,vUvs);
   // * only r channel, 只有r所以是红的 （1，0，0）是红的， multiplicative blending color1 * color2
   //* need to change the alpha chaneel, a 是 0的话，就显示 x 全部， a是1的话，就显示 y全部， a相当于是进程
-  gl_FragColor = mix(diffuseSample,overlaySample,overlaySample.w) ;
+  // gl_FragColor = mix(diffuseSample,overlaySample,overlaySample.w) ;
+  // * addressing mode
+  // * zoom in
+  vec2 uvs = vUvs / 10.0;
+  vec4 diffuseSample = texture2D(diffuse,uvs);
+  gl_FragColor = diffuseSample;
 }
