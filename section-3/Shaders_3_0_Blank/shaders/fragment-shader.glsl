17a9836b5c7384aecd492021dc3639cb0019fdb5
Marshall Chan <marshallchan666@outlook.com>
2023-04-18 03:54:44 +0800

    feat: know about importing image texture,texture 2d, uv cord, color * color, alpha

--- section-3/Shaders_3_0_Blank/shaders/fragment-shader.glsl
+++ section-3/Shaders_3_0_Blank/shaders/fragment-shader.glsl
@@ -1,4 +1,16 @@
-
+varying vec2 vUvs;
+// * for 2Dtexture 
+uniform sampler2D diffuse;
+uniform sampler2D overlay;
+// * 是个vector4 不是图像
+uniform vec4 tint;
 void main(void) {
-  gl_FragColor = vec4(0.0);
+  //  * 2D texture return rgba
+  // * flip out
+  // vec4 diffuseSample = texture2D(diffuse,(1.0 - vUvs));
+  vec4 diffuseSample = texture2D(diffuse,vUvs);
+  vec4 overlaySample = texture2D(overlay,vUvs);
+  // * only r channel, 只有r所以是红的 （1，0，0）是红的， multiplicative blending color1 * color2
+  //* need to change the alpha chaneel, a 是 0的话，就显示 x 全部， a是1的话，就显示 y全部， a相当于是进程
+  gl_FragColor = mix(diffuseSample,overlaySample,overlaySample.w) ;
 }
--- section-3/Shaders_3_0_Blank/shaders/vertex-shader.glsl
+++ section-3/Shaders_3_0_Blank/shaders/vertex-shader.glsl
@@ -1,6 +1,11 @@
 
+varying vec2 vUvs;
+
+
 void main() {
-  vec4 localPosition = vec4(position, 1.0);
+  vec4 localPosition = vec4( position.x, position.y,0.0, 1.0);
 
   gl_Position = projectionMatrix * modelViewMatrix * localPosition;
+  // * uv from the three.js
+  vUvs = uv;
 }
