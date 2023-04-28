1ed0bc326cf80570528270f5148e12959407c0c7
Marshall Chan <marshallchan666@outlook.com>
2023-04-29 06:38:59 +0800

    feat: implement the ambient and hemisphere light

--- section-6/Shaders_6_0_Blank/shaders/fragment-shader.glsl
+++ section-6/Shaders_6_0_Blank/shaders/fragment-shader.glsl
@@ -1,3 +1,5 @@
+varying vec3 vNormal;
+
 
 float inverseLerp(float v, float minValue, float maxValue) {
   return (v - minValue) / (maxValue - minValue);
@@ -9,7 +11,25 @@ float remap(float v, float inMin, float inMax, float outMin, float outMax) {
 }
 
 void main() {
-  vec3 colour = vec3(0.5);
+  vec3 baseColour = vec3(0.5);
+  vec3 lighting = vec3(0.0);
+  vec3 normal = normalize(vNormal);  
+  
+  // * ambient
+  vec3 ambient = vec3(0.5);
 
+  // * hemi light
+  vec3 skyColour = vec3(0.0,0.3,0.6);
+  vec3 groundColour = vec3(0.6,0.3,0.0);
+
+  // * normalize 只是abs 为 1,有可能出现 -1, 所以全部转为 0 ～ 1 范围
+  float hemiMix = remap(normal.y, -1.0,1.0,0.0,1.0);
+  // * 自然光
+  vec3 hemi = mix(groundColour, skyColour, hemiMix);
+
+  lighting = ambient * 0.0 + hemi;
+  vec3 colour  = baseColour * lighting;
+
+  // colour = normal;
   gl_FragColor = vec4(colour, 1.0);
 }
--- section-6/Shaders_6_0_Blank/shaders/vertex-shader.glsl
+++ section-6/Shaders_6_0_Blank/shaders/vertex-shader.glsl
@@ -2,7 +2,9 @@
 
 varying vec3 vNormal;
 varying vec3 vPosition;
-
+varying vec2 vUvs;
 void main() {	
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  vUvs = uv;
+  vNormal = (modelViewMatrix * vec4(normal,0.0)).xyz; 
 }
