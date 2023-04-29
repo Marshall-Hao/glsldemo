6d433955b67335e7ef99808e6c1ccd356bcb1975
Marshall Chan <marshallchan666@outlook.com>
2023-04-30 00:48:10 +0800

    feat: implement the much more realistic tone shading

--- section-6/Shaders_6_5_Toon/shaders/fragment-shader.glsl
+++ section-6/Shaders_6_5_Toon/shaders/fragment-shader.glsl
@@ -41,30 +41,27 @@ void main() {
   // Diffuse lighting
   vec3 lightDir = normalize(vec3(1.0, 1.0, 1.0));
   vec3 lightColour = vec3(1.0, 1.0, 0.9);
+  // * 每个 点 不同的 光照夹角强度
   float dp = max(0.0, dot(lightDir, normal));
 
-  // Fiddle with the values to get more or less lighting
-  // Toon
-  dp = smoothstep(0.5, 0.505, dp);
-
-  // 3 Colours
-  // dp = mix(0.5, 1.0, step(0.2, dp)) * step(0.05, dp);
+   // * Toon
+  dp *= smoothstep(0.5,0.505, dp);
 
   vec3 specular = vec3(0.0);
   vec3 diffuse = dp * lightColour;
-
-  // Specular
+ 
+  // * specular
   vec3 r = normalize(reflect(-lightDir, normal));
-  float phongValue = max(0.0, dot(viewDir, r));
-  phongValue = pow(phongValue, 128.0);
+  float phoneValue = max(0.0,dot(viewDir,r));
+  phoneValue = pow(phoneValue,128.0);
 
-  // Fresnel
-  float fresnel = 1.0 - max(0.0, dot(viewDir, normal));
-  fresnel = pow(fresnel, 2.0);
-  fresnel = step(0.7, fresnel);
+  // * fresnel
+  float fresnel = 1.0 - max(0.0,dot(viewDir,normal));
+  fresnel = pow(fresnel,2.0);
+  fresnel *= step(0.8,fresnel);
 
-  specular += phongValue;
-  specular = smoothstep(0.5, 0.51, specular);
+  specular += phoneValue;
+  specular = smoothstep(0.5,0.51,specular);
 
   lighting = hemi * (fresnel + 0.2) + diffuse * 0.8;
 
