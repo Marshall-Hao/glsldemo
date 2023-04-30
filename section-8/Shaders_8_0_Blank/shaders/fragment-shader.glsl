d228f746f28cbc487cf9a53b04066ae9ff3f83df
Marshall Chan <marshallchan666@outlook.com>
2023-04-30 16:37:40 +0800

    feat: simple shape with glsl, circle, line, box, hexagon

--- section-8/Shaders_8_0_Blank/shaders/fragment-shader.glsl
+++ section-8/Shaders_8_0_Blank/shaders/fragment-shader.glsl
@@ -46,8 +46,47 @@ vec3 drawGrid(vec3 colour, vec3 lineColour, float cellSpacing,float lineWidth) {
 
 }
 
+float sdfCircle(vec2 p,float r) {
+  return length(p) - r;
+}
+
+
+// * 一点 到 两点一线的距离 公式
+float sdfLine(vec2 p, vec2 a, vec2 b) {
+  vec2 pa = p - a;
+  vec2 ba = b - a;
+  // * line segemnt, the reflection pa on the line ab
+  float h = clamp(dot(pa,ba) / dot(ba,ba),0.0,1.0);
+  // *     P
+  // *   / | (real normal distance)   PA - (BA * h)
+  // * /___|_____
+  // *A  h factor  (BA * h)     B 
+  // * vector minus pa - ba * h(reflection factor) 
+  return length(pa - ba * h);
+}
+
+// * 右上角 b
+float sdfBox(vec2 p, vec2 b) {
+  vec2 d = abs(p) - b;
+
+  return length(max(d,0.0)) + min(max(d.x,d.y),0.0);
+}
+
+float sdHexagon( in vec2 p, in float r )
+{
+    const vec3 k = vec3(-0.866025404,0.5,0.577350269);
+    p = abs(p);
+    p -= 2.0*min(dot(k.xy,p),0.0)*k.xy;
+    p -= vec2(clamp(p.x, -k.z*r, k.z*r), r);
+    return length(p)*sign(p.y);
+}
+
+
+vec3 red = vec3(1.0, 0.0, 0.0);
+vec3 blue = vec3(0.0, 0.0, 1.0);
+
 void main() {
-  // *  move to center then reconstruct the res based on the screen
+  // *  move to center then reconstruct the res based on the screen , easily to access the number
   vec2 pixelCoords = (vUvs - 0.5) * resolution;
   
   vec3 colour = backgroundColour();
@@ -55,6 +94,20 @@ void main() {
   colour = drawGrid(colour, vec3(0.5),10.0,1.0);
   // * 每个格子相对 100 px, linewidth 相对2px
   colour = drawGrid(colour, vec3(0.0),100.0,2.0);
+
+  // float d = sdfCircle(pixelCoords,100.0);
+  // //* d 小于 0 就在圆圈内， 所以step 值就是 0 ，红色
+  // colour = mix(red, colour, step(0.0,d));
+
+  // float d = sdfLine(pixelCoords,vec2(0,0), vec2(200.0,-75.0));
+  // // * 距离在5内 才画
+  // colour = mix(red,colour, step(5.0,d));
+
+  // float d = sdfBox(pixelCoords, vec2(300.0,100.0));
+  // colour = mix(red,colour,step(0.0,d));
+
+  float d = sdHexagon(pixelCoords,300.0);
+  colour = mix(red,colour,step(0.0,d));
 
   gl_FragColor = vec4(colour, 1.0);
 }
